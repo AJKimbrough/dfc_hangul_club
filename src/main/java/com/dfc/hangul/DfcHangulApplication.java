32dd8c4baa055faad5901fef8059047205501e70
Amber Kimbrough (AJ) <94200525+AJKimbrough@users.noreply.github.com>
2025-08-25 12:27:39 -0500

    Initial Spring Boot app (web) with HomeController

---
+++ src/main/java/com/dfc/hangul/DfcHangulApplication.java
@@ -0,0 +1,13 @@
+package com.dfc.hangul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DfcHangulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DfcHangulApplication.class, args);
+	}
+
+}
